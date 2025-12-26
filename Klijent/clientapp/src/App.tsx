702f3dda5b514e7836d9d8c1878c1e262448fb8b
Ivan <Ivan@DESKTOP-V877DAD>
2025-12-26 17:32:58 +0100

    add prodaja route

--- Klijent/clientapp/src/App.tsx
+++ Klijent/clientapp/src/App.tsx
@@ -4,18 +4,20 @@ import HomePage from "./pages/HomePage";
 import ArtikliPage from "./pages/ArtikliPage";
 import ArtikliListPage from "./pages/ArtikliListPage";
 import ArtikalEditPage from "./pages/ArtikalEditPage";
+import ProdajaPage from "./pages/ProdajaPage";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <AppLayout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/artikli" element={<ArtikliPage />} />
-          <Route path="/artikli/lista" element={<ArtikliListPage />} />
-          <Route path="/artikli/:id/edit" element={<ArtikalEditPage />} />
-        </Routes>
-      </AppLayout>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <AppLayout>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/artikli" element={<ArtikliPage />} />
+                    <Route path="/artikli/lista" element={<ArtikliListPage />} />
+                    <Route path="/artikli/:id/edit" element={<ArtikalEditPage />} />
+                    <Route path="/prodaja" element={<ProdajaPage />} />
+                </Routes>
+            </AppLayout>
+        </BrowserRouter>
+    );
 }
